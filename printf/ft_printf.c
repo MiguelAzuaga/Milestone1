ddcd59954deeeaa4df2432337311a9a0185393bc
Miguel Queiros Ribeiro Azuaga <miguel.q.azuaga@gmail.com>
2025-04-26 10:27:53 +0100

    finished printf all clean B)

---
+++ printf/ft_printf.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mqueiros <mqueiros@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/17 10:26:24 by mqueiros          #+#    #+#             */
+/*   Updated: 2025/04/23 14:06:46 by mqueiros         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+int	ft_putflag(va_list args, const char c)
+{
+	if (c == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	else if (c == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	else if (c == 'p')
+		return (ft_putptr(va_arg(args, void *)));
+	else if (c == 'd' || c == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	else if (c == 'u')
+		return (ft_putuint(va_arg(args, unsigned int)));
+	else if (c == 'x' || c == 'X')
+		return (ft_puthex(va_arg(args, unsigned int), c == 'x'));
+	else if (c == '%')
+		return (ft_putchar('%'));
+	return (0);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	va_list	lst_arg;
+	size_t	i;
+	size_t	len;
+
+	if (!str)
+		return (-1);
+	va_start(lst_arg, str);
+	i = 0;
+	len = 0;
+	while (str[i])
+	{
+		if (str[i] == '%')
+			len += ft_putflag(lst_arg, str[++i]);
+		else
+			len += ft_putchar(str[i]);
+		i++;
+	}
+	va_end(lst_arg);
+	return (len);
+}
---
+++ printf/ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mqueiros <mqueiros@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/16 12:01:53 by mqueiros          #+#    #+#             */
+/*   Updated: 2025/04/26 09:42:23 by mqueiros         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+int	ft_printf(const char *str, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putptr(void *pointer);
+int	ft_putnbr(int nb);
+int	ft_putuint(unsigned int n);
+int	ft_puthex(unsigned long num, int lowercase);
+
+#endif
